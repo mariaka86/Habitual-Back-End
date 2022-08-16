e05bdb229d35834ad97f1b286ea957ebe4fc20aa
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-08-16 11:31:04 -0500

    setup middleware and env variables

--- server.js
+++ server.js
@@ -5,7 +5,21 @@
 
 'use strict'
 
+// importing required packages
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
+
+//telling our app to use express
+const app = express();
+
+//this is our middleware so our data is parsed correctly.
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded)({extended: true});
+
+//setting our env variables: mongo + port
+const PORT = process.env.PORT
+mongoose.connect(process.env.MONGO_URL);
+const db = mongoose.connection
